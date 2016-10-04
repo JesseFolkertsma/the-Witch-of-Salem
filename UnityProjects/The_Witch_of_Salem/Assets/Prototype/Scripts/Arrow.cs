0f8f1501fb40a2bd5d42ac9505e7f372c62dbc8d
JesseFolkertsma <jesse.folkertsma@gmail.com>
2016-10-04 14:54:00 +0200

    Added Arrow
    arrowwow

---
+++ UnityProjects/The_Witch_of_Salem/Assets/Prototype/Scripts/Arrow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arrow : MonoBehaviour {
+
+    Rigidbody rb;
+    public GameObject arrowHit;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(4, 3.5f, 0);
+    }
+	
+	void Update () {
+        transform.rotation = Quaternion.LookRotation(rb.velocity);
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        GameObject ah = Instantiate(arrowHit, transform.position, transform.rotation) as GameObject;
+        ah.transform.parent = col.transform;
+        Destroy(gameObject);
+    }
+}
