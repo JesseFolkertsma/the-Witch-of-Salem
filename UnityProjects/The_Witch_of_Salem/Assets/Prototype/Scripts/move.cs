2be3c6ea343cb38c5f6f029fd71518ba7268692a
JesseFolkertsma <jesse.folkertsma@gmail.com>
2016-10-04 17:15:52 +0200

    Did golem things

---
+++ UnityProjects/The_Witch_of_Salem/Assets/Prototype/Scripts/move.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+
+    public Vector3 movement;
+
+	void Update () {
+        transform.Translate(movement * Time.deltaTime);
+	}
+}
