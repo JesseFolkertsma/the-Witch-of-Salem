4ac3ac06a2dd791f96f5332b6553888800655615
JesseFolkertsma <jesse.folkertsma@gmail.com>
2016-10-31 16:41:16 +0100

    Added LoadingScreen

---
+++ UnityProjects/The_Witch_of_Salem/Assets/The Witch of Salem/Scripts/UI/LoadScreen.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LoadScreen : MonoBehaviour {
+
+    public Image loadbar;
+    public GameObject text;
+    AsyncOperation aSync;
+
+    void Start()
+    {
+        loadbar.fillAmount = 0;
+        text.SetActive(false);
+        aSync = SceneManager.LoadSceneAsync("Test Level");
+    }
+
+    void Update()
+    {
+        loadbar.fillAmount += Time.deltaTime * .5f;
+        if (aSync.progress >= 0.9f)
+        {
+            aSync.allowSceneActivation = false;
+
+            if (loadbar.fillAmount == 1)
+            {
+                text.SetActive(true);
+                if (Input.GetButtonDown("Jump"))
+                {
+                    aSync.allowSceneActivation = true;
+                }
+            }
+        }
+    }
+}
