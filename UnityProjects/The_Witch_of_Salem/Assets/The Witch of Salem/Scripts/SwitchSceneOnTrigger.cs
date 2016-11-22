2fb045606b0696171dd7c308344bbace3f7e0aca
JesseFolkertsma <jesse.folkertsma@gmail.com>
2016-11-22 09:22:06 +0100

    SwitchScene script

---
+++ UnityProjects/The_Witch_of_Salem/Assets/The Witch of Salem/Scripts/SwitchSceneOnTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SwitchSceneOnTrigger : MonoBehaviour {
+
+    public int sceneNumber;
+
+	void OnTriggerEnter(Collider col)
+    {
+        if(col.attachedRigidbody != null)
+        {
+            if(col.attachedRigidbody.tag == "Player")
+            {
+                SceneManager.LoadScene(sceneNumber);
+            }
+        }
+    }
+}
