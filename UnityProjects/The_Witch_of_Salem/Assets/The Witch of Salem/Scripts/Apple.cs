182440c51e2bc34fa6e4731fbdda526b52109d84
JesseFolkertsma <jesse.folkertsma@gmail.com>
2016-12-05 15:07:04 +0100

    Added witch to project

--- UnityProjects/The_Witch_of_Salem/Assets/The Witch of Salem/Scripts/Apple.cs
+++ UnityProjects/The_Witch_of_Salem/Assets/The Witch of Salem/Scripts/Apple.cs
@@ -3,12 +3,15 @@ using System.Collections;
 
 public class Apple : MonoBehaviour {
 
+    bool used;
+
 	void OnCollisionEnter(Collision col)
     {
-        if (col.collider.attachedRigidbody)
+        if (col.collider.attachedRigidbody && !used)
         {
             if(col.collider.attachedRigidbody.tag == "Player")
             {
+                used = true;
                 col.collider.attachedRigidbody.GetComponent<_Player>().apples++;
                 _UIManager.instance.UpdateUI();
                 _UIManager.instance.DisplayPopup("Picked up apple", 2f);
