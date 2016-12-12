abdc472afbcdc56e56bd00c4a7d9b7c0ea96d5e2
JesseFolkertsma <jesse.folkertsma@gmail.com>
2016-12-12 16:41:26 +0100

    Implemented Witch animatons

--- UnityProjects/The_Witch_of_Salem/Assets/The Witch of Salem/Scripts/Enemy/Boss.cs
+++ UnityProjects/The_Witch_of_Salem/Assets/The Witch of Salem/Scripts/Enemy/Boss.cs
@@ -2,10 +2,15 @@
 using System.Collections;
 
 public class Boss : Enemy {
+
+    public Animator anim;
+    public Rigidbody rb;
     
     public void BossStart()
     {
         player = FindObjectOfType<_PlayerBase>().transform;
+        anim = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody>();
     }
 
 }
