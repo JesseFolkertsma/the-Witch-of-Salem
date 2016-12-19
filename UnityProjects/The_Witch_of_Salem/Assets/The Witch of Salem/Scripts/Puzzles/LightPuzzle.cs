64555645219141ddabfb64e89acf34b578b9853b
Jochem Van Diepen <jochemiscool@hotmail.com>
2016-12-19 15:02:57 +0100

    Merge

--- UnityProjects/The_Witch_of_Salem/Assets/The Witch of Salem/Scripts/Puzzles/LightPuzzle.cs
+++ UnityProjects/The_Witch_of_Salem/Assets/The Witch of Salem/Scripts/Puzzles/LightPuzzle.cs
@@ -1,15 +1,44 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 using System.Collections;
 
 public class LightPuzzle : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    LightStone[] stones;
+    bool isDone = false;
+    public UnityEvent eventWhenDone;
+
+    void Start()
+    {
+        stones = GetComponentsInChildren<LightStone>();
+    }
+
+    public void CheckIfDone()
+    {
+        if (!isDone)
+        {
+            int actives = 0;
+            foreach (LightStone l in stones)
+            {
+                if (l.active)
+                {
+                    actives++;
+                }
+            }
+            if (actives == stones.Length)
+            {
+                FinishPuzzle();
+            }
+        }
+    }
+
+    public void FinishPuzzle()
+    {
+        isDone = true;
+        eventWhenDone.Invoke();
+        foreach(LightStone l in stones)
+        {
+            l.disable = true;
+        }
+    }
 }
---
+++ UnityProjects/The_Witch_of_Salem/Assets/The Witch of Salem/Scripts/Puzzles/RunePuzzle.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class RunePuzzle : MonoBehaviour {
+
+    public UnityEvent eventWhenCompleted;
+    RuneCircle[] circles;
+
+    bool completed = false;
+
+    void Start()
+    {
+        circles = GetComponentsInChildren<RuneCircle>();
+    }
+
+    void Update()
+    {
+        if (!completed)
+        {
+            if (circles[0].StanceIsRight && circles[1].StanceIsRight && circles[2].StanceIsRight)
+            {
+                FinishPuzzle();
+            }
+        }
+    }
+
+    public void FinishPuzzle()
+    {
+        completed = true;
+        eventWhenCompleted.Invoke();
+        foreach(RuneCircle r in circles)
+        {
+            r.disable = true;
+        }
+    }
+}
