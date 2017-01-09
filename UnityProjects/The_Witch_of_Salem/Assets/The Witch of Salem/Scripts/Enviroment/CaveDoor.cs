82159a729b949d4ee0ae5895e328c703ff8f4208
JesseFolkertsma <jesse.folkertsma@gmail.com>
2017-01-09 15:35:19 +0100

    OpenDoor script

---
+++ UnityProjects/The_Witch_of_Salem/Assets/The Witch of Salem/Scripts/Enviroment/CaveDoor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CaveDoor : MonoBehaviour {
+
+    bool puzzle1 = false;
+    bool puzzle2 = false;
+    bool puzzle3 = false;
+
+    public void FinishedPuzzle1()
+    {
+        puzzle1 = true;
+        Check();
+    }
+    public void FinishedPuzzle2()
+    {
+        puzzle2 = true;
+        Check();
+    }
+    public void FinishedPuzzle3()
+    {
+        puzzle3 = true;
+        Check();
+    }
+
+    void Check()
+    {
+        if(puzzle1 && puzzle2 && puzzle3)
+        {
+            GetComponent<Animator>().SetTrigger("OpenDoor");
+        }
+    }
+}
