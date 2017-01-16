964797928dbcbbe00ac6d39e897e8993266cc732
JesseFolkertsma <jesse.folkertsma@gmail.com>
2017-01-16 13:55:21 +0100

    almost_finished_witchfight

--- UnityProjects/The_Witch_of_Salem/Assets/The Witch of Salem/Scripts/Enemy/Boss.cs
+++ UnityProjects/The_Witch_of_Salem/Assets/The Witch of Salem/Scripts/Enemy/Boss.cs
@@ -5,11 +5,41 @@ public class Boss : Enemy {
 
     public Animator anim;
     public Rigidbody rb;
+    public GameObject ragdoll;
     
     public void BossStart()
     {
+        GEStart();
         player = FindObjectOfType<_PlayerBase>().transform;
         anim = GetComponentInChildren<Animator>();
         rb = GetComponent<Rigidbody>();
+    }
+
+    public void BossUpdate()
+    {
+        data.lives = lives;
+        if(lives < 1)
+        {
+            Die();
+        }
+    }
+
+    public override void Die()
+    {
+        if (!isDead)
+        {
+            base.Die();
+
+            if (ragdoll != null)
+            {
+                Instantiate(ragdoll, transform.position, transform.rotation);
+            }
+            else
+            {
+                Debug.LogWarning("Boss has no ragdoll!");
+            }
+
+            Destroy(gameObject);
+        }
     }
 }
